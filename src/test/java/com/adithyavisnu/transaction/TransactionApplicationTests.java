df9689164bd5df950b25e7000dbccb0772cf1293
Adithya Visnu <adithyavisnu@gmail.com>
2026-03-14 12:43:56 +0700

    update first commit

---
+++ src/test/java/com/adithyavisnu/transaction/TransactionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.adithyavisnu.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
